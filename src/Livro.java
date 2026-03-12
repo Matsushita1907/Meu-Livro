31eb4c8012b51aa25fac9b2a6fce962881487f4d
matsushita1907 <theus.matsu@gmail.com>
2026-03-12 09:45:24 -0300

    Classe livro criação da classe teste com o projeto

---
+++ src/Livro.java
@@ -0,0 +1,16 @@
+public class Livro {
+    //Atributos: Características do objeto
+    //usar padrão camelCase, não conter caracteres especiais
+    // tipo nomeAtributo
+
+    String titulo;
+    String autor;
+    double preco;
+    String editora;
+    int paginas;
+    String resumo;
+    String tipoCapa;
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+ public class HelloWorld {
+    public static void main(String[] args) {
+        //saida de dados
+        System.out.println("Hello World!");
+        System.out.println("Bom dia, sejam bem vindos");
+
+        //Entrada de dados
+    }
+}
